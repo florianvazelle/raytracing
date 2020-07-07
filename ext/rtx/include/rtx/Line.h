49c3330bf01621b355a8bc34713a7e93f8d6fe08
Wykio <antoine.thenevin@gmail.com>
2020-07-07 14:26:41 +0200

    add Line material & scale for duo

--- ext/rtx/include/rtx/Line.h
+++ ext/rtx/include/rtx/Line.h
@@ -1,5 +1,5 @@
-#ifndef H_CHECKBOARD
-#define H_CHECKBOARD
+#ifndef H_LINE
+#define H_LINE
 
 #include <rtx/Material.h>
 
@@ -10,18 +10,18 @@ namespace rtx {
 /**
  * Représente le matériau d'un objet.
  */
-struct Checkboard : Material {
+struct Line : Material {
   float scale;
 
-  Checkboard() : Material(), scale(1){};
-  Checkboard(const Color &ka, const Color &kd, const Color &ks,
-             const int &shininess = 0, const float &reflectivity = 0,
-             const float &refractivity = 0, const float &scale = 1)
+  Line() : Material(), scale(1){};
+  Line(const Color &ka, const Color &kd, const Color &ks,
+       const int &shininess = 0, const float &reflectivity = 0,
+       const float &refractivity = 0, const float &scale = 1)
       : Material(ka, kd, ks, shininess, reflectivity, refractivity),
         scale(scale){};
 
   const Color getAmbiante(const Point &impact) const {
-    float total = std::floor(impact.x / scale) + std::floor(impact.y / scale);
+    float total = std::floor(impact.x / scale);
     bool isEven = std::fmod(total, 2.0f) == 0.0f;
 
     if (isEven) {
