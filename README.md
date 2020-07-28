56961109e934fc79b9cae6a70a6128f1181d5976
florianvazelle <ponythugflorian@gmail.com>
2020-07-28 22:52:17 +0200

    remove SDL2 dependcy

--- README.md
+++ README.md
@@ -1,11 +1,5 @@
 # Raytracing
 
-- Uses [conan.io](https://conan.io) to obtain and setup libraries.
-
-```sh
-pip3 install conan
-```
-
 - Uses [CMake](https://cmake.org) for build system creation.
 
 ## Quickstart
--- src/GLTexture.cpp
+++ src/GLTexture.cpp
@@ -1,7 +1,8 @@
 #include "GLTexture.h"
 
-#include <SDL.h>
-#include <SDL_image.h>
+#define STB_IMAGE_WRITE_IMPLEMENTATION
+#include "stb_image_write.h"
+
 #include <iostream>
 #include <stdexcept>
 
@@ -43,27 +44,19 @@ const char *GLTexture::get_filename_ext(const char *filename) {
 }
 
 void GLTexture::save(char const *filename) {
-  SDL_Surface *out_surface;
-  out_surface = SDL_CreateRGBSurface(0, width, height, 32, 0, 0, 0, 0);
-  out_surface =
-      SDL_ConvertSurfaceFormat(out_surface, SDL_PIXELFORMAT_RGBA8888, 0);
-
-  SDL_LockSurface(out_surface);
-  auto pixels = static_cast<uint8_t *>(out_surface->pixels);
+  uint8_t pixels[size() * 4];
   for (int i = 0; i < size(); ++i) {
     int j = 4 * i;
-    pixels[j] = 255.0f;
-    pixels[j + 1] = textureData[i].b * 255.0f;
-    pixels[j + 2] = textureData[i].g * 255.0f;
-    pixels[j + 3] = textureData[i].r * 255.0f;
+    pixels[j + 0] = textureData[i].r * 255.0f;
+    pixels[j + 1] = textureData[i].g * 255.0f;
+    pixels[j + 2] = textureData[i].b * 255.0f;
+    pixels[j + 3] = 255.0f;
   }
-  SDL_UnlockSurface(out_surface);
 
   const char *extension = get_filename_ext(filename);
-  if (strcmp(extension, "png"))
-    IMG_SavePNG(out_surface, filename);
-  else if (strcmp(extension, "jpg"))
-    IMG_SaveJPG(out_surface, filename, 100);
-
-  SDL_FreeSurface(out_surface);
+  if (strcmp(extension, "png")) {
+    stbi_write_png(filename, width, height, 4, pixels, 4 * width);
+  } else if (strcmp(extension, "jpg")) {
+    stbi_write_jpg(filename, width, height, 4, pixels, 100);
+  }
 }
