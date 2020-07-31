71dae69390397476ecebfde0e4cd4b059a53faf0
florianvazelle <ponythugflorian@gmail.com>
2020-07-31 20:31:23 +0200

    refacto with shared_ptr

--- ext/rtx/include/rtx/Scene.h
+++ ext/rtx/include/rtx/Scene.h
@@ -1,6 +1,7 @@
 #ifndef H_SCENE
 #define H_SCENE
 
+#include <memory>
 #include <vector>
 
 #include <rtx/Camera.h>
@@ -22,43 +23,35 @@ struct Scene {
   bool useShadow;
   bool useAmbiantOcclusion;
 
-  std::vector<Object *> objects;
-  std::vector<Light *> lights;
+  std::vector<std::shared_ptr<Object>> objects;
+  std::vector<std::shared_ptr<Light>> lights;
 
   Color getBackground() const;
   Color getAmbiant() const;
   int nbLights() const;
-  const Light *getLight(int index) const;
-  Object *getClosestIntersection(const Ray &ray, Point &impact) const;
+  const std::shared_ptr<Light> getLight(int index) const;
+  std::shared_ptr<Object> getClosestIntersection(const Ray &ray, Point &impact) const;
 
   Color castRayForPixel(const Camera &cam, float i, float j) const;
   Color castRay(const Ray &ray, int raycast = 0) const;
 
-  Color performLighting(const Ray &ray, const Object &obj, const Point &impact,
-                        int raycast) const;
+  Color performLighting(const Ray &ray, const Object &obj, const Point &impact, int raycast) const;
 
   /* Lighting stuff */
   bool isInShadow(const Light &light, const Point &impact) const;
   Color getAmbiantLighting(const Object &obj, const Point &impact) const;
-  Color getDiffuseLighting(const Ray &ray, const Object &obj,
-                           const Point &impact) const;
-  Color getSpecularLighting(const Ray &ray, const Object &obj,
-                            const Point &impact) const;
+  Color getDiffuseLighting(const Ray &ray, const Object &obj, const Point &impact) const;
+  Color getSpecularLighting(const Ray &ray, const Object &obj, const Point &impact) const;
 
-  Color getReflectiveRefractive(const Ray &ray, const Object &obj,
-                                const Point &impact, int raycast) const;
-  Color getReflectivity(const Ray &ray, const Object &obj, const Point &impact,
-                        int raycast) const;
-  Color getRefractivity(const Ray &ray, const Object &obj, const Point &impact,
-                        int raycast) const;
+  Color getReflectiveRefractive(const Ray &ray, const Object &obj, const Point &impact, int raycast) const;
+  Color getReflectivity(const Ray &ray, const Object &obj, const Point &impact, int raycast) const;
+  Color getRefractivity(const Ray &ray, const Object &obj, const Point &impact, int raycast) const;
 
-  float schlickFresnel(const Vector &I, const Vector &N, float n1,
-                       float n2) const;
+  float schlickFresnel(const Vector &I, const Vector &N, float n1, float n2) const;
   Vector reflect(const Vector &I, const Vector &N) const;
   Vector refract(const Vector &I, const Vector &N, float n1, float n2) const;
 
-  float ambientOcclusion(const Ray &ray, const Object &obj, const Point &impact,
-                         int raycast) const;
+  float ambientOcclusion(const Ray &ray, const Object &obj, const Point &impact, int raycast) const;
 };
 
 } // namespace rtx
